3d822a7f172d2b2c517fa5b2e2a089e9f08a9f94
nikozdev <nikozdev@nikozdev.net>
2023-07-24 15:38:50 -0700

    !env!build!system!cmake!help!module!refactor!iface!require!vnum
    [env/CMakeLists.txt]
    > changed the default install prefix to home directory
    > > hopefully it runs well on windows
    > make executable and test it options are now set to 1 by default again
    > specify verison of libasist that we use for better version control
    > hide some cmake options from the config interface marking it as advanced
    > renamed oProjConf back to oProjConfPick
    [env/CMakeLists.txt]
    [env/dCmake.dir/dHelp.dir/fOpt.cmake] [env/dCmake.dir/dHelp.dir/fRegex.cmake]
    > changed the cmake version from "3.3" to "3.3...3.8"
    > > warnings on windows that versions below like 3.8 are gonna be deprecated

--- env/dCmake.dir/dHelp.dir/fOpt.cmake
+++ env/dCmake.dir/dHelp.dir/fOpt.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.3)
+cmake_minimum_required(VERSION 3.3...3.8)
 include_guard(GLOBAL)
 # actions
 function(fOptEnum oOptPick vOptList vOptDesc)
--- env/dCmake.dir/dHelp.dir/fRegex.cmake
+++ env/dCmake.dir/dHelp.dir/fRegex.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.20)
+cmake_minimum_required(VERSION 3.3...3.8)
 include_guard(GLOBAL)
 # actions
 function(fRegexIgnoreRBracket vRegex)
